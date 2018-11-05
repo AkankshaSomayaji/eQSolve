5e6dc92c4e016bddb7a566d5e69c18effe15fe4e
kvorhu <akhilsundaram@yahoo.in>
2018-11-05 09:21:56 +0530

    FrontEnd Init

---
+++ eQSolve/eQSolve/views.py
@@ -0,0 +1,31 @@
+"""
+Routes and views for the flask application.
+"""
+
+from datetime import datetime
+from flask import render_template
+from eQSolve import app
+
+
+
+@app.route('/')
+def index():
+    """Renders the main page"""
+    return render_template(
+        'main.html',
+        title='eQSolve',
+        year = datetime.now().year,
+        backgroundColor = "#ff7f50",
+        headerColor = "#2f3542"
+        )
+
+@app.route('/solve')
+def solve():
+    """Renders the main page"""
+    return render_template(
+        'solve.html',
+        title='eQSolve',
+        year = datetime.now().year,
+        backgroundColor = "#d72323",
+        headerColor = "#2a363b"
+        )
